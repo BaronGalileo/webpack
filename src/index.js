42392c9f42e4f6884b8c554befa4f7e8ed89e685
Dimka <barongalileo@gmail.com>
2024-03-12 23:16:23 +0300

    testDe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import    { sum as sum_my } from "./math.js";
-kjksjaldj
 import "./style.css";
-
-document.write("Hello, world!");
+zzxzx
+document.write("Hello, world!");xz
 alert('Привет!');
 
 const sum = (a,b) => a * b;
 
-console.log(sum_my(5, 3));S
+console.log(sum_my(5, 3));
 
 console.log(sum(5, 3));
